756dfb7809ea4aa7e11de18ca556da295dc9515e
mumei404 <lozld30@gmail.com>
2023-08-11 00:46:11 +0900

    Prisma

--- __tests__/sample.test.ts
+++ __tests__/sample.test.ts
@@ -1,4 +1,5 @@
 import  {calculate} from '../src/sample'
+import { Prisma } from '@prisma/client'
 it('one', () => {
     const result = calculate(1, 2, false)
     expect(result).toBe(1)
@@ -7,4 +8,9 @@ it('one', () => {
 it('two', () => {
     const result = calculate(3, 2, false)
     expect(result).toBe(1)
+})
+
+it('three', () => {
+    console.log(new Prisma.Decimal(2))
+    expect(1).toBe(1)
 })
